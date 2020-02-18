2db18edc52b50a875e5359147277efdf27640f61
UK9302 <jaeuk20@naver.com>
2020-02-18 10:55:22 +0900

    UI추가

---
+++ VxLoadMenu/CVxParsor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace VxLoadMenu
+{
+    public class CVxParsor
+    {
+        public static String VxGetTodayMenu()
+        {
+            String strTodayMenu = String.Empty;
+
+            try
+            {
+                var timeoutInMilliseconds = 5000;
+                var uri = new Uri("http://mini.snu.kr/cafe/today/");
+                var doc = Supremes.Dcsoup.Parse(uri, timeoutInMilliseconds);
+
+                var menuDiv = doc.Select("div#main.br_menu2");
+                var menuName = menuDiv.Select("td");
+
+                int iIndex = 0;
+
+                for (int i = 0; i < menuName.Count; i++)
+                {
+                    if (menuName[i].ToString().Contains(@"/cafe/miniinfo/w"))
+                    {
+                        iIndex = i;
+                        break;
+                    }
+                }
+                strTodayMenu = Regex.Replace(menuName[iIndex + 1].ToString(), "<[^>]*>", "\n").Replace("&amp;", ",");
+                strTodayMenu = strTodayMenu.Replace("45", "4,500");
+                strTodayMenu = strTodayMenu.Replace("50", "5,000");
+                strTodayMenu = strTodayMenu.Replace("55", "5,500");
+                strTodayMenu = strTodayMenu.Replace("\n\n", "\n");
+
+                
+                //System.Windows.MessageBox.Show(strTodayMenu);
+            }
+            catch (Exception e)
+            {
+                //System.Windows.MessageBox.Show("ERROR : " + e.Message);
+            }
+
+            return strTodayMenu;
+        }
+    }
+}
---
+++ VxLoadMenu/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VxLoadMenu
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            tblTest.Text = CVxParsor.VxGetTodayMenu();
+        }
+
+        private BitmapImage VxMakeBitmapImageFromDisk(String strUri, UriKind uriKind = UriKind.RelativeOrAbsolute, Image imgStoreTarget = null)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.CacheOption = BitmapCacheOption.None;
+            bitmapImage.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+            bitmapImage.UriSource = new Uri(strUri, uriKind);
+            bitmapImage.EndInit();
+            bitmapImage.Freeze();
+            if (imgStoreTarget != null)
+                imgStoreTarget.Source = bitmapImage;
+
+            return bitmapImage;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //imgBack.Source = VxMakeBitmapImageFromDisk("pack://application:,,,/VxLoadMenu;component/background.png");
+        }
+    }
+}
